0e47788deff0483e7a0f1c4a51211cf8ebc20889
Mostafa-elleithy123 <elleithymostafa708@gmail.com>
2023-10-05 13:48:50 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
-int main(vold)
+#include <stdio.h>
+
+/**
+ *main - this is the main fuction
+ *
+ * Return always 0 
+ */ 
+
+int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle);
-	return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
