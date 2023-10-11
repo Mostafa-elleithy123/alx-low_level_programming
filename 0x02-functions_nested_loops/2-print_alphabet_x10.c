8ed140d3ce9f2c959c081b78fb07193d279eb4c8
Mostafa-elleithy123 <elleithymostafa708@gmail.com>
2023-10-11 16:21:19 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10- Makealphabet x10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+
+	char ch;
+	int i;
+	i = 0;
+	while (i < 10)
+	{
+		for (ch = 'a'; ch <= 'z'; chh++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
