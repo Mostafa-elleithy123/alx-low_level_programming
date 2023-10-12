20d4f7cdc5af26822243e1c05fd319fe76f1f8e1
Mostafa-elleithy123 <elleithymostafa708@gmail.com>
2023-10-11 17:06:03 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,13 @@
 /**
  * _isalpha - Checks for alphabetic character
  * @c: The character to be checked
- * Return: 1 for alphabtic character or 0 for anything else
+ * Return: 1 for alphabetic character or 0 for anything else
  */
+
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
-	{
+if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+{
 return (1);
 }
 return (0);
